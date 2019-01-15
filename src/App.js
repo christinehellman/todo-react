3d3add1daf493ad6bbcdebc66734d41f5de264a5
Christine Hellman <39390812+christinehellman@users.noreply.github.com>
2019-01-15 10:57:11 -0500

    initial upload

---
+++ src/App.js
@@ -0,0 +1,28 @@
+import React, {Component} from 'react';
+//import Conditional from './components/Conditional';
+import MainContent from './components/MainContent';
+
+
+
+
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+    }
+   
+  }
+ 
+
+
+  render() {
+
+    return (
+      <div>
+        <MainContent />
+      </div>
+    )
+  }
+};
+
+export default App;
